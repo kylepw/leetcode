4d862ea5eb6eefd19a54a540a68444f8dd09cd7d
Kyle <kylepw@gmail.com>
2019-10-11 13:43:16 +0900

    Solved 07

--- 07_reverse_int.py
+++ 07_reverse_int.py
@@ -20,8 +20,23 @@ integer overflows.
 import unittest
 
 
-def reverse(x: int) -> int:
-    pass
+def reverse_num(x):
+    if x > 2**31 - 1 or x < -2**31:
+        return 0
+    if -10 < x < 10:
+        return x
+    reversed_x = str(x)[::-1]
+    if reversed_x.startswith('0'):
+        i = 0
+        while reversed_x[i] == '0':
+            i += 1
+        reversed_x = reversed_x[i:]
+    if reversed_x.endswith('-'):
+        reversed_x = '-' + reversed_x[:-1]
+    result = int(reversed_x)
+    if result > 2**31 - 1 or result < -2**31:
+        return 0
+    return int(reversed_x)
 
 
 class TestReverse(unittest.TestCase):
@@ -34,7 +49,7 @@ class TestReverse(unittest.TestCase):
 
     def test_reverse(self):
         for x, expected in self.data.items():
-            self.assertEqual(reverse(x), expected)
+            self.assertEqual(reverse_num(x), expected)
 
 
 if __name__ == "__main__":
