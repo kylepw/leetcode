8a2093a6a78fcfcade8e8f2ec67e60c442e4d9dc
Kyle <kylepw@gmail.com>
2019-10-11 19:00:19 +0900

    solve 08

--- 08_string_to_int.py
+++ 08_string_to_int.py
@@ -31,12 +31,32 @@ import unittest
 
 
 def my_atoi(s):
-    pass
+    if len(s) < 1:
+        return 0
+    s = list(s.strip())
+    if not str.isdigit(s[0]) and s[0] not in ('-', '+'):
+        return 0
+    is_neg = True if s[0] == '-' else False
+    if s[0] in ('-', '+'):
+        del s[0]
+    i = x = 0
+    while x < len(s) and str.isdigit(s[x]):
+        i = i * 10 + ord(s[x]) - ord('0')
+        x += 1
+    if x < 1:
+        # No numbers
+        return 0
+    if i > 2**31 - 1:
+        if is_neg:
+            return -2**31
+        return 2**31 - 1
+    return -i if is_neg else i
 
 
 class TestMyAtoi(unittest.TestCase):
     def setUp(self):
         self.data = {
+            '+1': 1,
             '42': 42,
             '   -42': -42,
             '4193 with words': 4193,
@@ -47,3 +67,7 @@ class TestMyAtoi(unittest.TestCase):
     def test_my_atoi(self):
         for arg, expected in self.data.items():
             self.assertEqual(my_atoi(arg), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
